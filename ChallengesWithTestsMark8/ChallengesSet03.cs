a533c2d40480dcba7a07394d725d65a9c39f4069
Jamie Norman <jamiedeez@gmail.com>
2022-03-24 11:40:30 -0400

    Challenges Week 3

--- ChallengesWithTestsMark8/ChallengesSet03.cs
+++ ChallengesWithTestsMark8/ChallengesSet03.cs
@@ -7,47 +7,152 @@ namespace ChallengesWithTestsMark8
     {
         public bool ArrayContainsAFalse(bool[] vals)
         {
-            throw new NotImplementedException();
+            //handle the null
+            if (vals == null)
+            {
+                return false;
+            }
+
+            //loops to find any "false" values
+            for (int i = 0; i < vals.Length; i++)
+            {
+                if (vals[i] == false)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+
         }
 
         public bool IsSumOfOddsOdd(IEnumerable<int> numbers)
         {
-            throw new NotImplementedException();
+            //handle the null
+            if (numbers == null)
+            {
+                return false;
+            }
+
+            var sum = 0;
+
+            foreach (int num in numbers)
+            {
+                if ((num % 2) != 0)
+                {
+                    sum += num;
+                }
+            }
+
+            return sum % 2 != 0 ? true : false;
         }
 
         public bool PasswordContainsUpperLowerAndNumber(string password)
         {
-            throw new NotImplementedException();
+            var upper = false;
+            var lower = false;
+            var number = false;
+
+            foreach (char c in password)
+            {
+                if (Char.IsUpper(c))
+                {
+                    upper = true;
+                    if (upper == true)
+                    {
+                        break;
+                    }
+                }
+            }
+            foreach (char c in password)
+            {
+                if (Char.IsLower(c))
+                {
+                    lower = true;
+
+                    if (lower == true)
+                    {
+                        break;
+                    }
+
+                }
+            }
+
+            foreach (char c in password)
+            {
+                if (Char.IsNumber(c))
+                {
+                    number = true;
+
+                    if (number == true)
+                    {
+                        break;
+                    }
+                }
+            }
+
+            if (upper == true && lower == true && number == true)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
         }
 
         public char GetFirstLetterOfString(string val)
         {
-            throw new NotImplementedException();
+            return val[0];
         }
 
         public char GetLastLetterOfString(string val)
         {
-            throw new NotImplementedException();
+            var endNum = val.Length - 1;
+            return val[endNum];
         }
 
         public decimal Divide(decimal dividend, decimal divisor)
         {
-            throw new NotImplementedException();
+            if (divisor == 0)
+            {
+                return divisor;
+            }
+            else
+            {
+                return dividend / divisor;
+            }
         }
 
         public int LastMinusFirst(int[] nums)
         {
-            throw new NotImplementedException();
+            var endNum = nums.Length - 1;
+            return (nums[endNum]) - (nums[0]);
         }
 
         public int[] GetOddsBelow100()
         {
-            throw new NotImplementedException();
+            var numList = new List<int>();
+
+            for (int i = 0; i < 100; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    numList.Add(i);
+                }
+            }
+
+            return numList.ToArray();
+
         }
 
         public void ChangeAllElementsToUppercase(string[] words)
         {
-            throw new NotImplementedException();
+            for (int i = 0; i < words.Length; i++)
+            {
+                words[i] = words[i].ToUpper();
+            }
         }
     }
 }
